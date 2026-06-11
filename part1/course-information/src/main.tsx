687485d7dd65f912cb6e76c56ad063468e76783a
Johnny Astudillo <johnnyastudilloso@gmail.com>
2026-06-11 17:54:18 +0200

    final solution

--- part1/course-information/src/main.tsx
+++ part1/course-information/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") || document.createElement("div");
+
+ReactDOM.createRoot(root).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
